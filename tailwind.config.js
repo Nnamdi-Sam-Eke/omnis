82fca5e77cae99c7bbb33f2f1525915e982ce10e
Nnamdi Sam Eke <69424455+Nnamdi-Sam-Eke@users.noreply.github.com>
2025-04-24 11:25:37 +0100

    Adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Ensure 'class' mode is enabled
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
